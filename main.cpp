d6bc4c8ef3732eb9d9bc111fe27522fcc47a2d86
akashkapoordev <akashkapoordev@gmail.com>
2024-10-23 13:14:50 -0400

    Choosing Your Legendary Pokemon!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 using namespace std;
 
+enum PokemonChoice {
+    Charmander,
+    Bulbasaur,
+    Squirtle,
+    InvalidChoice
+};
+
 int main()
 {
+    PokemonChoice chosen_pokemon = PokemonChoice::InvalidChoice;
     string player_name;
     string chosen_pokemon;
 
@@ -31,26 +39,39 @@ int main()
     switch (choice)
     {
     case 1:
-        chosen_pokemon = "Charmander";
-        cout << "Professor Oak: You've chosen " << chosen_pokemon << "! A fiery partner with a burning passion!\n";
+        chosen_pokemon = PokemonChoice::Charmander;
         break;
     case 2:
-        chosen_pokemon = "Bulbasaur";
-        cout << "Professor Oak: " << chosen_pokemon << " is your choice! A steady companion with deep roots.\n";
+        chosen_pokemon = PokemonChoice::Bulbasaur;
         break;
     case 3:
-        chosen_pokemon = "Squirtle";
+        chosen_pokemon = PokemonChoice::Squirtle;
+        break;
+    default:
+        chosen_pokemon = PokemonChoice::InvalidChoice;
+        break;
+    }
+
+    //Response from the choice
+    switch (chosen_pokemon)
+    {
+    case Charmander:
+        cout << "Professor Oak: You've chosen " << chosen_pokemon << "! A fiery partner with a burning passion!\n";
+        break;
+    case Bulbasaur:
+        cout << "Professor Oak: " << chosen_pokemon << " is your choice! A steady companion with deep roots.\n";
+        break;
+    case Squirtle:
         cout << "Professor Oak: " << chosen_pokemon << ", a fine choice! Always calm under pressure.\n";
         break;
     default:
         cout << "Professor Oak: Hmm, it seems you've made an unusual choice... Let's go with a special one!\n";
-        chosen_pokemon = "Pikachu";
+        chosen_pokemon = PokemonChoice::Charmander;
         cout << "Professor Oak: Ah, " << chosen_pokemon << " joins you as a surprise companion! How electrifying!\n";
         break;
-    }
 
     // Concluding the first chapter
-    cout << "Professor Oak: " << chosen_pokemon << " will be your faithful companion on this grand journey, " << player_name << "!\n";
+    cout << "Professor Oak: " << (chosen_pokemon == PokemonChoice::Charmander? "Charmander" : chosen_pokemon == PokemonChoice::Bulbasaur ? "Bulbasaur" : chosen_pokemon == PokemonChoice::Squirtle ? "Squirtle" : "Invalid Choice") << " will be your faithful companion on this grand journey, " << player_name << "!\n";
     cout << "Professor Oak: Now, your adventure begins! The world of Pokémon awaits you with endless possibilities!\n";
 
     return 0;
