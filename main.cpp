120ddb8187bf9b458e3f6f66bb1bfce0e69193d6
akashkapoordev <akashkapoordev@gmail.com>
2024-10-23 12:09:22 -0400

    Your First Pokemon!

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@ using namespace std;
 
 int main()
 {
-	string player_name;
-	cout << "Enter your name : " << endl;
-	cin >> player_name;
-	cout << "Great Start" + player_name + ", looks like you have understood the main() function properly now!" << endl;
+	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+	cout << "You can choose one of the following Pokémon:\n";
+	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+	cout << "Which Pokémon would you like to choose? Enter the number: ";
+
+	int choice;
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		count << "You chose Bulbasaur! A wise choice.\n";
+		break;
+	case 2:
+		cout << "You chose Charmander! A fiery choice.\n";
+		break;
+	case 3:
+		cout << "You chose Squirtle! A cool choice.\n";
+		break;
+	default:
+		cout << "Invalid choice. Please restart the game.\n";
+		break;
+	}
 	return 0;
 }
