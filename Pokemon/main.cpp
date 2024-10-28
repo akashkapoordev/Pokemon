60b7546c8d1aa7a618483dc7b0818e672d481723
akashkapoordev <akashkapoordev@gmail.com>
2024-10-27 20:29:11 -0400

    Create Main Game Loop

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -205,6 +205,57 @@ public:
     }
 };
 
+void GameLoop(Player& player)
+{
+    int choice;
+    bool  keepPlaying = true;
+
+    while (keepPlaying)
+    {
+        clearConsole();
+        cout << "\nWhat would you like to do next - [" << player.name << "]\n";
+        cout << "1. Battle Wild Pokémon\n";
+        cout << "2. Visit PokeCenter\n";
+        cout << "3. Challenge Gyms\n";
+        cout << "4. Enter Pokémon League\n";
+        cout << "5. Quit\n";
+        cout << "Enter your choice (1-5): ";
+        cin >> choice;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        switch (choice)
+        {
+        case 1:
+            cout<< "You look around... but all the wild Pokemon are on vacation. Maybe try again later?\\n";
+            break;
+        case 2:
+            cout<< "You head to the PokeCenter, but Nurse Joy is out on a coffee break. Guess your Pokemon will have to tough it out for now!\\n";
+            break;
+        case 3:
+            cout<< "You march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break!\\n";
+            break;
+        case 4:
+            cout<< "You boldly step towards the Pokemon League... but the gatekeeper laughs and says, 'Maybe next time, champ!'\\n";
+            break;
+        case 5:
+            cout<< "You try to quit, but Professor Oak's voice echoes: 'There's no quitting in Pokemon training!'\\n";
+            cout<< "Are you sure you want to quit? (y/n): ";
+            char quitChoice;
+            cin >> quitChoice;
+            if (quitChoice == 'Y' || quitChoice == 'y')
+            {
+                keepPlaying = false;
+            }
+            break;
+        default:
+            cout << "Invalid Input \\n";
+        
+            break;
+        }
+        waitforEnter();
+    }
+    cout << "Goodbye, " << player.name << "! Thanks for playing!\n";
+}
+
 int main()
 {
     Pokemon Charmander("Charmander", PokemonType::GRASS, 100);
@@ -222,7 +273,7 @@ int main()
     professor_oak.explainMainQuest(player);
 
     // Placeholder for where the game loop will start
-    cout << "\n[Placeholder for the Game Loop]\n";
+    GameLoop(player);
 
     //// Concluding the first chapter
     //cout << "\n***************************************" << endl;
